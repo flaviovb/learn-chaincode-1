7280f2aa77656be19afa9d5f89a9c6ac7f0d8f9a
flaviovb <flaviovb@gmail.com>
2016-12-14 11:51:59 -0500

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -70,8 +70,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	// Handle different functions
 	if function == "read" { //read a variable
 		var msg = "Poderia estar de ferias, mas estou aqui estudando Hyperledger!"		
-		return []byte(msg), nil 		
-		//return t.read(stub, args)
+		//return []byte(msg), nil 		
+		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
 
